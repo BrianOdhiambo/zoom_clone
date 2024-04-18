6539184161eefeb5650520090a84fb8e7b3d692c
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 11:38:10 +0300

    Added root folder in app

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,22 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
+import TimeDisplay from "@/components/TimeDisplay";
+
+const Home = () => {
+
+
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting at: 10:00 PM</h2>
+
+          <TimeDisplay />
+        </div>
+      </div>
+
+      <MeetingTypeList />
+    </section>
+  )
+}
+
+export default Home
